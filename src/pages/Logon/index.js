bb8d129c7eafc36b52fd3dc4ca4eab317b5f547f
Gustavo Silva <gustavo@MacBook-Pro-de-Gustavo.local>
2020-03-26 11:11:16 -0300

    Created Login-page

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './styles.css';
+import { FiLogIn } from 'react-icons/fi';
 
+import herosImg from '../../assets/img/heroes.png';
+import logoImage from '../../assets/img/logo.svg'
 
 export default function Logon(){
     return(
-        <h1>Hello World</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImage} alt="Logo Be The Hero"/>
+
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua id"/>
+                    <button className="button" type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={herosImg} alt="Heroes"/>
+        </div>
     );
 }
